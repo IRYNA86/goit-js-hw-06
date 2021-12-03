f24441ff38087f360810f44294095aabcd8533ed
Iryna Hudymenko <iryn4ik09@gmail.com>
2021-12-03 23:53:33 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listUl = document.querySelector("#ingredients");
+const listFirst = document.createElement("li");
+listFirst.classList.add('item');
+listFirst.append('Potatoes');
+const listSecond = document.createElement("li");
+listSecond.classList.add('item');
+listSecond.append('Mushrooms',);
+const listThird = document.createElement("li");
+listThird.classList.add('item');
+listThird.append('Garlic',);
+const listFourth = document.createElement("li");
+listFourth.classList.add('item');
+listFourth.append('Tomatos',);
+const listFifth = document.createElement("li");
+listFifth.classList.add('item');
+listFifth.append('Herbs',);
+const listSixth = document.createElement("li");
+listSixth.classList.add('item');
+listSixth.append('Condiments',);
+listUl.prepend(listFirst, listSecond, listThird, listFourth, listFifth, listSixth);
+console.log(listUl);
